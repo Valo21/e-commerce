84e19db4a5658c477a562c0c72992c91a0a11bcc
Valentín <61363314+Valo21@users.noreply.github.com>
2024-03-17 13:01:15 -0300

    chore(frontend): update cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'node:path';
-import * as session from 'express-session';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "node:path";
+import * as session from "express-session";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://e-commerce-qjyl.onrender.com/'],
     methods: ['GET', 'POST', 'PATCH'],
     credentials: true,
   });
